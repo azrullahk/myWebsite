2adc1a47e9dc84a848e979485fb8a276027a21dc
azrullahk <azrullahkainage@gmail.com>
2022-04-22 16:47:15 +0900

    versi 1

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 0
+
+setInterval(() => {
+    count ++
+
+    if (count%2!=0) {
+        console.log("peransssss")
+    } 
+    else if (count%10==0) {
+        console.log("andreasss eka adii")
+    }
+    else {
+        console.log(count)
+    }
+}, 1000)
